2afb2a8d029a8a62658050f1100c8a5918393814
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-22 22:41:20 +0900

    feat: 로또 번호 생성기 추가

---
+++ src/main/java/com/example/junit5testcode/util/LottoNumberGenerator.java
@@ -0,0 +1,30 @@
+package com.example.junit5testcode.util;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class LottoNumberGenerator {
+
+    public List<Integer> generate(int money) {
+
+        if (!isValidMoney(money)) {
+            throw new RuntimeException("올바른 금액이 아닙니다");
+        }
+        return generate();
+    }
+
+    private boolean isValidMoney(int money) {
+        return money == 1000;
+    }
+
+    private List<Integer> generate() {
+        return new Random()
+                .ints(1, 45 + 1)
+                .distinct()
+                .limit(6)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+}

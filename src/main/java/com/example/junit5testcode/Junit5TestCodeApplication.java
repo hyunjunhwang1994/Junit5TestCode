eaf73eccf43ab439c31ff905777020414f1f9f4f
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-22 20:41:57 +0900

    Initial commit

---
+++ src/main/java/com/example/junit5testcode/Junit5TestCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit5testcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5TestCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5TestCodeApplication.class, args);
+    }
+
+}
